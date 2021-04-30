25ac8779edc04869baed0cf317d627d8cefd36ff
arjunMee <70093722+arjunMee@users.noreply.github.com>
2021-04-30 15:35:58 +0530

    tour app

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://course-api.com/react-tours-project';
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTour = async () => {
+    setLoading(true);
+
+    try {
+      let promise = await fetch(url);
+      let respose = await promise.json();
+      setLoading(false);
+      setTours(respose);
+    } catch (err) {
+      setLoading(false);
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    fetchTour();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />;
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  );
 }
 
-export default App
+export default App;
